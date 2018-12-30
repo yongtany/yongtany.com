cbf0afc390d1353f4f43ade4168cc789dbb47da2
Charles4th <yongn1494@gmail.com>
2018-12-30 21:54:06 +0900

    Mongo and mongoose setting

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,11 +1,24 @@
+const User = require('../models/user');
+
 module.exports = {
     signUp: async(req, res, next) => {
-        console.log('contents of req.value.body', req.value.body);
-        console.log('UsersController.signUp() called!');
+      const { email, password } = req.value.body;
+
+      // Check if there is a user iwth the same email
+      const foundUser = await User.findOne({ email });
+      if(foundUser) {
+        res.status(403).send({ error: 'Email is already i use'});
+      };
+
+      // Create a new user
+      const newUser = new User({ email, password });
+      await newUser.save();
+      // Respond with token
+      res.json({ user: 'created' });
     },
 
     signIn: async(req, res, next) => {
-        console.log('UsersController.signIn() called!');
+        // Generate token
     },
 
     secret: async(req, res, next) => {
