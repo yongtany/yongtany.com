70337b71cc208ea7425c81334385eef89996442b
Charles4th <yongn1494@gmail.com>
2018-12-30 22:05:54 +0900

    Add jsonwebtoken

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,4 +1,15 @@
-const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const User = require('models/user');
+const { JWT_SECRET } = require('config');
+
+signToken = user => {
+  return jwt.sign({
+    iss: 'NomadYong',
+    sub: user.id,
+    iat: new Date().getTime(), // current time
+    exp: new Date().setDate(new Date().getDate() +1) // current time + 1 day ahead
+  }, JWT_SECRET);
+}
 
 module.exports = {
     signUp: async(req, res, next) => {
@@ -13,12 +24,17 @@ module.exports = {
       // Create a new user
       const newUser = new User({ email, password });
       await newUser.save();
+
+      // Generate the token
+      const token = signToken(newUser);
+
       // Respond with token
-      res.json({ user: 'created' });
+      res.json({ token });
     },
 
     signIn: async(req, res, next) => {
         // Generate token
+
     },
 
     secret: async(req, res, next) => {
