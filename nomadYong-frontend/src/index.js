56c167be4ab81e31f2933864463824598c615eb0
Charles4th <yongn1494@gmail.com>
2019-01-04 16:32:22 +0900

    Configure redux

--- nomadYong-frontend/src/index.js
+++ nomadYong-frontend/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configure from './store/configure';
+
 import registerServiceWorker from './registerServiceWorker';
 
-import App from './components/App';
+const store = configure();
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter> ,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider> ,
   document.querySelector('#root'));
 registerServiceWorker();
