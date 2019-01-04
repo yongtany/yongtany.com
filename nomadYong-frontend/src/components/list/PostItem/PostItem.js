2ca12372928ae2fd6b746512a556795f9e693819
Charles4th <yongn1494@gmail.com>
2019-01-05 02:22:53 +0900

    Post card image error

--- nomadYong-frontend/src/components/list/PostItem/PostItem.js
+++ nomadYong-frontend/src/components/list/PostItem/PostItem.js
@@ -3,18 +3,19 @@
 import React from 'react';
 import styles from './PostItem.scss';
 import classNames from 'classnames/bind';
-import { Link } from 'react-router-dom';
+
 
 const cx = classNames.bind(styles);
 
-const url = 'https://picsum.photos/740/420/?random'
 
 const PostItem = () => (
   <div className={cx('post-item')}>
-    <img src={url} alt={'photo'} />
+    <div className={cx('photo')} />
     <div className={cx('content')}>
-      <span className={cx('pre-title')}>2019-01-04</span>
       <span className={cx('title')}>Title</span>
+      <p className={cx('sub')}>
+        <span className={cx('published')}>By nomadyong</span> | <span className={cx('created')}>On 2019-01-04</span>
+      </p>
       <p className={cx('text')}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
       <div className={cx('tags')}>
         <a to="#">#태그</a>
--- nomadYong-frontend/src/components/list/RecomPostList/RecomPostList.js
+++ nomadYong-frontend/src/components/list/RecomPostList/RecomPostList.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
 import styles from './RecomPostList.scss';
 import classNames from 'classnames/bind';
@@ -5,14 +6,29 @@ import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
+const url='https://picsum.photos/740/420/?random';
+
 const RecomPostItem = () => (
-  <div>
-    Recommand Post
+  <div className={cx('recompost-item')}>
+    <div className={cx('column')}>
+      <img src={url} alt={'photo'} />
+    </div>
+    <div className={cx('column')}>
+      <div className={cx('content')}>
+        <span className={cx('title')}>Title</span>
+        <span className={cx('author')}>By author</span>
+        <div className={cx('tags')}>
+          <a to="#">#태그</a>
+          <a to="#">#태그</a>
+          <a to="#">#태그</a>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
 const RecomPostList = () => (
-  <div className={cx('post-list')}>
+  <div className={cx('recompost-list')}>
     <RecomPostItem />
     <RecomPostItem />
     <RecomPostItem />
