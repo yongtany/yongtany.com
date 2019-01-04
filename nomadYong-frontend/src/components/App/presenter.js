5fc6e3c9327085e4a95939f491fc61ef9d402f07
Charles4th <yongn1494@gmail.com>
2019-01-04 16:57:59 +0900

    Configure routing

--- nomadYong-frontend/src/components/App/presenter.js
+++ nomadYong-frontend/src/components/App/presenter.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import Navigation from '../Navigation';
-import './styles.scss';
+import { Switch, Route } from 'react-router-dom';
+import {
+  HomePage,
+  ProfilePage,
+  ListPage,
+  PostPage,
+  EditorPage,
+  NotFoundPage
+} from '../../pages';
 
-const App = props => (
-  <Navigation />
-);
+const App = () => {
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/post" component={ListPage} />
+                <Route path="/post/:page" component={ListPage} />
+                <Route path="/tag/:tag/:page?" component={ListPage} />
+                <Route path="/post/:id" component={PostPage} />
+                <Route path="/editor" component={EditorPage} />
+                <Route path="/profile" component={ProfilePage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
+    );
+};
 
 export default App;
---
+++ nomadYong-frontend/src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as HomePage } from './HomePage';
+export { default as ProfilePage } from './ProfilePage';
+export { default as ListPage } from './ListPage';
+export { default as PostPage } from './PostPage';
+export { default as EditorPage } from './EditorPage';
+export { default as NotFoundPage } from './NotFoundPage';
