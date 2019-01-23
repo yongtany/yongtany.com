fa8fefab3365d008c5f7623df92beea306a10aef
yongtany <yongn1494@gmail.com>
2019-01-23 21:04:40 +0900

    Ongoing update CRA_v2

--- yongtany-frontend/src/index.js
+++ yongtany-frontend/src/index.js
@@ -5,9 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import configure from './store/configure';
 
-
-
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import 'styles/base.scss';
 
 const store = configure();
@@ -18,5 +16,9 @@ ReactDOM.render(
       <AppContainer />
     </BrowserRouter>
   </Provider> ,
-  document.querySelector('#root'));
-registerServiceWorker();
+  document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
