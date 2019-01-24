359cf2fc8ddedc7dd6e96aa93511d431c4204fc6
yongtany <yongn1494@gmail.com>
2019-01-24 17:08:54 +0900

    Heroku deploy settings

--- yongtany-backend/src/index.js
+++ yongtany-backend/src/index.js
@@ -17,13 +17,26 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-
 // Routes
 app.use('/users', require('routes/users.routes'));
 app.use('/posts', require('routes/posts.routes'));
 
 // Start the server
 const port = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('yontany-frontend/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path= require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'yontany-frontend', 'build', 'index.html'));
+  });
+};
+
 
 app.listen(port, err => {
   if(err) {
