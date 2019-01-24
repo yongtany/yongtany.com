8c143e2e391f8ffafb77a249d448c4ab77e42bc3
yongtany <yongn1494@gmail.com>
2019-01-24 17:16:22 +0900

    heroku deploy

--- yongtany-backend/src/index.js
+++ yongtany-backend/src/index.js
@@ -27,13 +27,13 @@ const port = process.env.PORT || 5000;
 if(process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
-  app.use(express.static('yontany-frontend/build'));
+  app.use(express.static('../yontany-frontend/build'));
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
   const path= require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'yontany-frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../yontany-frontend', 'build', 'index.html'));
   });
 };
 
