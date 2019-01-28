c6fbdac2b0432e0fd9e14871bbbfde927b55e4b2
yongtany <yongn1494@gmail.com>
2019-01-28 16:03:32 +0900

    Heroku deploy

--- yongtany-frontend/src/store/modules/index.js
+++
@@ -1,6 +0,0 @@
-export { default as editor } from './editor';
-export { default as list } from './list';
-export { default as post } from './post';
-export { default as auth } from './auth';
-export { default as modal } from './modal';
-export { penderReducer as pender } from 'redux-pender';
